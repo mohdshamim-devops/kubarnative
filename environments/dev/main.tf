56d91b0a60fe5e544bf8e3041aa71396f724293f
shamimss <mohd.shamim38@gmail.com>
2026-09-02 14:48:36 +0530

    main update

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,10 +7,12 @@ module "acr" {
   source = "../../modules/acr"
 
   acr_name = var.acr_name
+  depends_on = [module.resource_group]
 }
 
 module "aks" {
   source = "../../modules/aks"
 
 aks_name = var.aks_name
+depends_on = [module.resource_group]
 }
